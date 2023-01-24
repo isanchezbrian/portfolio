d21ddee8ced401a25785a8f1eac2e3dc5fa6c317
brians97 <isanchez.brian@gmail.com>
2023-01-23 16:34:04 -0800

    added links to projects and socials

--- pages/_document.js
+++ pages/_document.js
@@ -4,6 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head />
+      <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@200;400;700&family=Poppins:wght@200;400;500;700&display=swap" rel="stylesheet" />
       <body>
         <Main />
         <NextScript />
