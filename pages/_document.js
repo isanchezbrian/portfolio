b8f46a9584f745f8c2a4d7b6de5bd57ad87f8d8d
brians97 <isanchez.brian@gmail.com>
2023-01-23 16:37:18 -0800

    added links to projects and socials

--- pages/_document.js
+++ pages/_document.js
@@ -3,8 +3,9 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
-      <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@200;400;700&family=Poppins:wght@200;400;500;700&display=swap" rel="stylesheet" />
+      <Head>
+        <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@200;400;700&family=Poppins:wght@200;400;500;700&display=swap" rel="stylesheet" />
+      </Head>
       <body>
         <Main />
         <NextScript />
