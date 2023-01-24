6a41996ac6a43f6f4c5affbc21140ef584aed32d
brians97 <isanchez.brian@gmail.com>
2023-01-23 16:48:39 -0800

    added links to projects and socials

--- pages/_document.js
+++ pages/_document.js
@@ -5,6 +5,7 @@ export default function Document() {
     <Html lang="en">
       <Head>
         <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@200;400;700&family=Poppins:wght@200;400;500;700&display=swap" rel="stylesheet" />
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/devicons/devicon@v2.15.1/devicon.min.css" />
       </Head>
       <body>
         <Main />
